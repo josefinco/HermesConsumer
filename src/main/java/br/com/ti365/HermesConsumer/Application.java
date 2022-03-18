d8264e311cc983c3b2fcefc49962293076c24825
José Finco <josefinco_@hotmail.com>
2022-03-18 11:59:41 -0300

    Adicionar função para escrita de eventos Kafka na fila RabbitMQ

--- src/main/java/br/com/ti365/HermesConsumer/Application.java
+++ src/main/java/br/com/ti365/HermesConsumer/Application.java
@@ -1,19 +1,21 @@
 package br.com.ti365.HermesConsumer;
 
-import br.com.ti365.HermesConsumer.service.impl.POMKafkaConsumer;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.ConnectionFactory;
+
+import br.com.ti365.HermesConsumer.service.POMKafkaService;
+import br.com.ti365.HermesConsumer.service.RabbitService;
+import br.com.ti365.HermesConsumer.service.impl.POMKafkaServiceImpl;
+import br.com.ti365.HermesConsumer.service.impl.RabbitServiceImpl;
 
 public class Application {
 
 	public static void main(String[] args) {
+		RabbitService rabbitmq = new RabbitServiceImpl();
+		POMKafkaService consumirdorEventoKafka = new POMKafkaServiceImpl();
+		Channel rabbitMQChannel= rabbitmq.connectionFactory(new ConnectionFactory());
+		consumirdorEventoKafka.startConsumerKafka(rabbitMQChannel);
 
-		Application application = new Application();
-		application.iniciar();
-		
-	}
-	
-	private void iniciar() {
-		POMKafkaConsumer consumirdorEvento = new POMKafkaConsumer();
-		consumirdorEvento.executar();
 	}
 
 }

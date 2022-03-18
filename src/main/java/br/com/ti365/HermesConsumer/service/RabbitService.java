9a32c4d00c6d74f6a5fa08929f6a59042ba651a4
José Finco <josefinco_@hotmail.com>
2022-03-18 11:57:24 -0300

    Adicionar interface Rabbit

--- src/main/java/br/com/ti365/HermesConsumer/service/RabbitService.java
+++ src/main/java/br/com/ti365/HermesConsumer/service/RabbitService.java
@@ -1,10 +1,18 @@
 package br.com.ti365.HermesConsumer.service;
 
 import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.ConnectionFactory;
 
-public interface RabbitConnectionService {
+public interface RabbitService {
 
-	Channel connectionFactory();
+	/*
+	 * Create connection with RabbitMQ Server
+	 */
+	Channel connectionFactory(ConnectionFactory factory);
 	
+	
+	/*
+	 * Publish new menssage on Queue
+	 */
 	void publish(Channel channel, String message);
 }

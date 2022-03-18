33b53c2735bd5a90f75db105d12b6a904d3062c8
José Finco <josefinco_@hotmail.com>
2022-03-18 11:56:08 -0300

    Adicionar funções propertie

--- src/main/java/br/com/ti365/HermesConsumer/service/PropertyService.java
+++ src/main/java/br/com/ti365/HermesConsumer/service/PropertyService.java
@@ -1,9 +1,24 @@
 package br.com.ti365.HermesConsumer.service;
 
 import java.util.Properties;
+import com.rabbitmq.client.ConnectionFactory;
 
 public interface PropertyService {
 	
-	 Properties loadProperties();
+	
+	/*
+	 * Get all propiertes from application.properties file 
+	 */
+	 Properties loadFileProperties();
+	 
+	 /*
+	  * Get Kafka Properties from file
+	  */
+	 Properties loadKafkaProperties();
+	 	 
+	 /*
+	  * Get rabbitMQ Factory with properties parameters injected
+	  */
+	 ConnectionFactory loadRabbitProperties(Properties properties);
 	
 }
